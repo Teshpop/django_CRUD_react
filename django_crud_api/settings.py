64ba9705b0ca7b527fd7b01dfaec9b36acddd718
Pedro Tello <Tello.3810@outlook.com>
2025-02-04 18:20:20 -0600

    allowed

--- django_crud_api/settings.py
+++ django_crud_api/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-zaz3=ivc*aqoc@g=yttdkl0cw4k2ba1+$)0^w&$lojsj67$gm8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('FRONTEND_URL', '').split(",")
 
 
 # Application definition
